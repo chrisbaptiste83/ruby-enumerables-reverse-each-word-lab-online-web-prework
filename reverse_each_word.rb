f4e365ab179fc22138f044e95a370c1645931531
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-06 05:52:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,8 @@
 def rereverse_each_word(string)
-  
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
