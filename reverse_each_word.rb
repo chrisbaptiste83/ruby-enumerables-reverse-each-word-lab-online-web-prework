855a6aea086ab4b411d13423ec4dfec85528d68e
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-06 05:54:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def rereverse_each_word(string)
-  original_array = string.split(" ")
+  sentence1 = string.split(" ")
   return_array = []
   original_array.each do|string|
     return_array << string.reverse
